e1f39fd131fae5645b7431e0c9bf412ec1437c73
kimiabeh <110967737+kimiabeh@users.noreply.github.com>
2024-12-02 23:08:43 +0330

    js code

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -1,22 +1,7 @@
 const iconPlus = document.getElementById("icon");
+const showAnswer = document.querySelector(".answer");
 iconPlus.addEventListener("click", () => {
   console.log("hi");
+  textContent = `<div>${showAnswer}</div>`;
   document.querySelector(".answer").textContent;
 });
-// ***********damn chatgpt!!*************
-// const button = document.querySelectorAll(".item").forEach((item) => {
-//   item.addEventListener("click", () => {
-//     const toggleIcon = item.getElementById(".icon");
-//     const answer = item.nextElementSibling; // The answer div comes right after the question div
-
-//     // Toggle the answer visibility
-//     if (answer.style.display === "none" || answer.style.display === "") {
-//       answer.style.display = "block";
-//       toggleIcon.textContent = "-";
-//     } else {
-//       answer.style.display = "none";
-//       toggleIcon.textContent = "+";
-//     }
-//   });
-// });
-// ************************
