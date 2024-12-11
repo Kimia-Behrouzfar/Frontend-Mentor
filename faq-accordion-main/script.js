607e6814bc1edbb2e1be3784d26bc0725b1aed4f
kimiabeh <110967737+kimiabeh@users.noreply.github.com>
2024-12-11 23:53:54 +0330

    fixing

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -1,7 +1,35 @@
-const iconPlus = document.getElementById("icon");
-const showAnswer = document.querySelector(".answer");
-iconPlus.addEventListener("click", () => {
-  console.log("hi");
-  textContent = `<div>${showAnswer}</div>`;
-  document.querySelector(".answer").textContent;
-});
+// const icons = document.querySelectorAll(".icon");
+// const answers = document.querySelectorAll(".answer");
+
+// icons.forEach((icon) => {
+//   icon.addEventListener("DOMContentLoaded", () => {
+//     if (answers.style.display === "none") {
+//       answers.style.display = "block";
+//     } else {
+//       answers.style.display = "none";
+//     }
+//   if (answers.style.visibility === "hidden") {
+//     answers.style.visibility = "visible";
+//   } else {
+//     answers.style.visibility = "hidden";
+//   }
+//   });
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+  const icons = document.getElementById("icon");
+  const answers = document.getElementById("answer");
+
+  icons.forEach(icon) => {
+    icon.addEventListener("click", ()=> {
+      if (answers.style.display === "none") {
+        answers.style.display = "block";
+      } else {
+        answers.style.display = "none";
+      }
+    // });
+  }});
+
+
+console.log(icons);
+console.log(answers);
