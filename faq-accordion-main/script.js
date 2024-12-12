6431ea791c24e4de4080af1935e7773f7b9c70ae
kimiabeh <110967737+kimiabeh@users.noreply.github.com>
2024-12-12 22:29:15 +0330

    write js code

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -17,19 +17,57 @@
 // });
 
 // document.addEventListener("DOMContentLoaded", function () {
-  const icons = document.getElementById("icon");
-  const answers = document.getElementById("answer");
+// const icons = document.getElementById("icon");
+// const answers = document.getElementById("answer");
 
-  icons.forEach(icon) => {
-    icon.addEventListener("click", ()=> {
+// icons.forEach(icon) => {
+//   icon.addEventListener("click", ()=> {
+//     if (answers.style.display === "none") {
+//       answers.style.display = "block";
+//     } else {
+//       answers.style.display = "none";
+//     }
+
+// })};
+
+// const minusIcon = document.querySelectorAll(".minus");
+// const answers = document.querySelectorAll(".answer");
+// const plusIcon = document.querySelectorAll(".plus").forEach((answer) => {
+//   answer.addEventListener("click", () => {
+// plusIcon.replace(minusIcon);
+// const answr = answer.parentElement.nextElementSibling;
+
+// if (answr.style.display === "none") {
+//   answers.style.display = "block";
+// icon.textContent = answers;
+// }
+//  else {
+// answr.style.display = "none";
+// icon.textContent = plusIcon;
+// }
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const answers = document.getElementById("answer");
+  const plusIcon = document.querySelectorAll(".plus");
+  plusIcon.forEach((icon) => {
+    icon.addEventListener("click", () => {
       if (answers.style.display === "none") {
         answers.style.display = "block";
-      } else {
-        answers.style.display = "none";
+        icon.textContent = answers;
       }
-    // });
-  }});
 
+      // if (answers.classList.contains("show")) {
+      //   answers.classList.remove("show");
+      //   plusIcon.textContent = "+";
+      // } else {
+      //   answers.classList.add("show");
+      //   plusIcon.textContent = "-";
+      // }
+    });
+  });
+});
 
-console.log(icons);
-console.log(answers);
+// console.log(icons);
+// console.log(answers);
