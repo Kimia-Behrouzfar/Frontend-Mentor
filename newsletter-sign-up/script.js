42d1cafd72075a4581b0874494e1f2f580f6e634
kimiabeh <110967737+kimiabeh@users.noreply.github.com>
2024-12-17 17:53:14 +0330

    done without responsive

--- newsletter-sign-up/script.js
+++ newsletter-sign-up/script.js
@@ -1,9 +1,7 @@
 const message = document
   .querySelector(".Subscribe")
   .addEventListener("click", function (e) {
-    e.preventDefault(); // Prevent form submission
-    // Clear previous error
-    // errorElement.textContent = "";
+    e.preventDefault();
 
     const remove = document.getElementById("log-in");
     remove.style.display = "none";
@@ -11,22 +9,13 @@ const message = document
 
     const add = document.getElementById("msg");
     if (add) {
-      // Check if the element exists
       add.style.display = "inline-block";
+
       console.log("Cart element shown:", add);
     } else {
       console.error("Cart element not found.");
     }
   });
 
-// Check if the input is empty
-// if (!emailInput) {
-//   errorElement.textContent = "Email field cannot be empty.";
-//   return;
-// }
-
-// Check if the input has exactly 8 characters
-// if (emailInput.length >= 8) {
-//   errorElement.textContent = "Email must be exactly 8 characters.";
-//   return;
-// }
+// I don't write code about condition of email and responsive!!!!
+// that's it just know!
