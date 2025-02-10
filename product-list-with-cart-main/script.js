55b323b09de458fe3101abe8934aa3691ac1cc13
kimiabeh <110967737+kimiabeh@users.noreply.github.com>
2025-02-10 19:39:56 +0330

    add to cart done!

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -1,6 +1,23 @@
-const addCart = document.getElementById("btn-cart");
-addCart.addEventListener("click", () => {
-  const createTag = document.createElement("div");
+const minusIcon = document.getElementById("minus");
+const plusIcon = document.getElementById("plus");
+const quantityDisplay = document.getElementById("btn-cart");
+let quantity = 0;
 
-  // "hsl(14, 86%, 42%)";
+plusIcon.addEventListener("click", () => {
+  if (quantity === 0) {
+    quantityDisplay.textContent = 1;
+  } else {
+    quantityDisplay.textContent = parseInt(quantityDisplay.textContent) + 1;
+  }
+  quantity = parseInt(quantityDisplay.textContent);
+});
+
+minusIcon.addEventListener("click", () => {
+  if (quantity > 1) {
+    quantityDisplay.textContent = parseInt(quantityDisplay.textContent) - 1;
+    quantity = parseInt(quantityDisplay.textContent);
+  } else if (quantity === 1) {
+    quantityDisplay.textContent = "Add to Cart";
+    quantity = 0;
+  }
 });
